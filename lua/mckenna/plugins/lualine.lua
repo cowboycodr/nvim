f515c2ec63c5e24c00d86289b304c9474151f9b2
Kian McKenna <kian.p.m.007@gmail.com>
2026-03-18 11:17:50 -0500

    delete opencode; update lualine

--- lua/mckenna/plugins/lualine.lua
+++ lua/mckenna/plugins/lualine.lua
@@ -8,6 +8,9 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "gruvbox",
+
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 			},
 		})
 	end,
